8a9df9afac83a468fa0645cb2a7961637952a750
MarTorb <marius.torbjornsen@gmail.com>
2022-04-12 14:53:48 +0200

    Add requirements and fix svd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     df = load_data("active1000")
    
     print("\nRecommendation based on collaborative filtering with SVD...")
-    #collaborative_filtering_svd(df)
+    collaborative_filtering_svd(df)
 
     print("\nRecommendation based on content based filtering with top-k and KNN...")
     content_based_filtering(df)
